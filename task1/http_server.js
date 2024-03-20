f858173c8b44372c5465a2fdcbc66635c3b7efef
desktop <psomail@yandex.ru>
2024-03-20 22:12:38 +0300

    task1 ready

--- task1/http_server.js
+++ task1/http_server.js
@@ -1,10 +1,30 @@
 const http = require('http')
+const port = 8181
+let rootPageCounter = 0
+let aboutPageCounter = 0
+
 const server = http.createServer((request, response) => {
-        console.log("test")
+        console.log(`${new Date()} INFO: request received: ${request.url}`)
+
+        if(request.url === '/') {
+            response.writeHead(200, {
+                'Content-Type': 'text/html; charset=UTF-8',
+            })
+            response.end(`<h1>Корневая страница</h1><p>Просмотров: ${++rootPageCounter}</p><a href="/about">Ссылка на страницу /about</a>`)
+        } else if(request.url === '/about') {
+            response.writeHead(200, {
+                'Content-Type': 'text/html; charset=UTF-8',
+            })
+            response.end(`<h1>Страница about</h1><p>Просмотров: ${++aboutPageCounter}</p><a href="/">Ссылка на страницу /</a>`)
+        } else {
+            response.writeHead(404, {
+                'Content-Type': 'text/html; charset=UTF-8'
+            });
+            response.end('<h1>Страница не найдена!</h1>')
+        }
     }
 )
 
-const port = 8181
 server.listen(port, () => {
-    console.log('INFO: ')
+    console.log(`${new Date()} INFO: server started, port: ${port}`)
 })
